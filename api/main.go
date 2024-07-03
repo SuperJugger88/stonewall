ea47bc2310fa20b3e5480aa53e1c36a09b114103
Andrey Ivanov <SuperJugger88@gmail.com>
2024-07-04 00:46:49 +0300

    feature: added db connection

--- api/main.go
+++ api/main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"api/routes"
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"os"
 )
 
-func main() {
+func initRouter() {
 	router := gin.Default()
 
-	router.GET("/", routes.MainPage)
+	router.GET("/", routes.GetMainPage)
 
-	router.Run(":80")
+	err := router.Run(os.Getenv("APP_URL"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initDatabaseConnection() {
+	config, err := pgx.ParseConfig(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := pgx.ConnectConfig(context.Background(), config)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+}
+
+func main() {
+	initRouter()
+	initDatabaseConnection()
 }
