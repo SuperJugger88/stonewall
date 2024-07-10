388846986a3b20db5556bebc51c0197d1e031d86
Andrey Ivanov <SuperJugger88@gmail.com>
2024-07-11 00:34:04 +0300

    feature: added inserts to table

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	crdbpgx "github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgxv5"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"log"
 	"os"
@@ -36,12 +37,23 @@ func initTable(ctx context.Context, tx pgx.Tx) error {
 	return nil
 }
 
+func insertRows(ctx context.Context, tx pgx.Tx, accts [4]uuid.UUID) error {
+	// Insert four rows into the "accounts" table.
+	log.Println("Creating new rows...")
+	if _, err := tx.Exec(ctx,
+		"INSERT INTO accounts (id, balance) VALUES ($1, $2), ($3, $4), ($5, $6), ($7, $8)", accts[0], 250, accts[1], 100, accts[2], 500, accts[3], 300); err != nil {
+		return err
+	}
+	return nil
+}
+
 func initDatabaseConnection() {
 	config, err := pgx.ParseConfig(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	config.RuntimeParams["application_name"] = "$ docs_simplecrud_gopgx"
 	conn, err := pgx.ConnectConfig(context.Background(), config)
 	if err != nil {
 		panic(err)
@@ -51,6 +63,20 @@ func initDatabaseConnection() {
 	err = crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		return initTable(context.Background(), tx)
 	})
+
+	var accounts [4]uuid.UUID
+	for i := 0; i < len(accounts); i++ {
+		accounts[i] = uuid.New()
+	}
+
+	err = crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+		return insertRows(context.Background(), tx, accounts)
+	})
+	if err == nil {
+		log.Println("New rows created.")
+	} else {
+		log.Fatal("error: ", err)
+	}
 }
 
 func main() {
